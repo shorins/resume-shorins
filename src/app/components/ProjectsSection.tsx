116783bafe75cb19b5d6b744211aa1c619f946e5
MightyShorin <mighty.shorin@ya.ru>
2025-06-27 02:42:06 +0300

    кнопка collab адаптив fix

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -30,7 +30,7 @@ const ProjectsSection = () => {
                 href={colabLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="rounded-full px-4 py-2 text-sm font-semibold flex items-center gap-2 bg-slate-100 text-slate-600 hover:bg-orange-500 hover:text-white transition-colors duration-200"
+                className="mt-4 md:mt-0 self-start whitespace-nowrap rounded-full px-4 py-2 text-sm font-semibold flex items-center gap-2 bg-slate-100 text-slate-600 hover:bg-orange-500 hover:text-white transition-colors duration-200"
               >
                 <ColabIcon className="w-5 h-5" />
                 <span>Посмотреть ноутбук</span>
